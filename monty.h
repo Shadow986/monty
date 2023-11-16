a1c643b78fa71ed7c3adfb1574add2ea01cf9e91
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-11-16 07:20:32 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -41,7 +41,8 @@ typedef struct instruction_s
 
 /* PROTOTYPES */
 
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, char **argv);
+push(stack, line_number, argv);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 
