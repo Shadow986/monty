eec016e542e737c7a4fd6a307b1b31a752045dc1
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-16 16:22:03 +0300

    Applied modifications to files: instructions.c

--- instructions.c
+++ instructions.c
@@ -8,23 +8,11 @@
  * sentinel structure containing NULL for both the opcode and function.
  */
 instruction_t instruction_set[] = {
-	/**
-	 * push - Pushes an element onto the stack
-	 */
 	{"push", push, NULL},
 
-	/**
-	 * pall - Prints all values on the stack
-	 */
 	{"pall", NULL, pall},
 
-	/**
-	 * pint - Prints the value at the top of the stack
-	 */
 	{"pint", NULL, pint},
 
-	/**
-	 * Sentinel structure to mark the end of the array
-	 */
 	{NULL, NULL, NULL}
 };
