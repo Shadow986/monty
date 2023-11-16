8b59a56c6367b1009e59bac497eb0ffd0be10c7d
Shadow986 <archie986jr@gmail.com>
2023-11-15 20:01:52 -0800

    0-push_pall.c

---
+++ 0-push_pall.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * push - push an element to the stack
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (stack == NULL)
+		return;
+
+	n = atoi(argv[1]);
+	if (n == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	push_stack(stack, n);
+}
+
+/**
+ * pall - print all the values on the stack, starting from the top
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL)
+		return;
+
+	print_stack(*stack);
+}
