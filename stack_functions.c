a560882f52665684f53c59f86d1a877d0aac161e
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-11-17 00:51:45 +0200

    Update stack_functions.c

--- stack_functions.c
+++ stack_functions.c
@@ -1,24 +1,30 @@
 #include "monty.h"
 
-void stack_pop(stack_t **stack) {
+void stack_pop(stack_t **stack)
+{
 	stack_t *temp = *stack;
 
-	if (*stack == NULL) {
+	if (*stack == NULL)
+	{
 		fprintf(stderr, "Error: can't pop an empty stack\n");
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
 	*stack = (*stack)->next;
-	if (*stack != NULL) {
+	if (*stack != NULL)
+	{
 		(*stack)->prev = NULL;
 	}
 	free(temp);
 }
 
-void stack_push(stack_t **stack, int value) {
+void stack_push(stack_t **stack, int value)
+{
 	stack_t *new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL) {
+
+	if (new_node == NULL)
+	{
 		fprintf(stderr, "Error: memory allocation failed\n");
 		exit(EXIT_FAILURE);
 	}
@@ -27,7 +33,8 @@ void stack_push(stack_t **stack, int value) {
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
-	if (*stack != NULL) {
+	if (*stack != NULL)
+	{
 		(*stack)->prev = new_node;
 	}
 
