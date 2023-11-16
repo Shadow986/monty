d124e47589189e6497cd462728050b22175b4a84
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-16 22:32:58 +0300

    Updating files

--- stack_functions.c
+++ stack_functions.c
@@ -1,12 +1,14 @@
 #include "monty.h"
 
 void stack_pop(stack_t **stack) {
+	stack_t *temp = *stack;
+
 	if (*stack == NULL) {
 		fprintf(stderr, "Error: can't pop an empty stack\n");
 		exit(EXIT_FAILURE);
 	}
 
-	stack_t *temp = *stack;
+	temp = *stack;
 	*stack = (*stack)->next;
 	if (*stack != NULL) {
 		(*stack)->prev = NULL;
